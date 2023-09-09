53ce5457e934dfcc51cd29a0fa49b0daf30dcf80
KimGyeJeong <pmwkdo@gmail.com>
2023-09-09 23:12:47 +0900

    #JAVA #1001

---
+++ src/Stage_1/문제10998/Main.java
@@ -0,0 +1,16 @@
+package Stage_1.문제10998;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] str = br.readLine().split(" ");
+
+        System.out.println(Integer.parseInt(str[0]) * Integer.parseInt(str[1]));
+    }
+}
