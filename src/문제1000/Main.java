4561d4b6319de5c9aafb2d48332cbcc27a63c0da
KimGyeJeong <pmwkdo@gmail.com>
2023-09-09 22:15:21 +0900

    #JAVA #1000

--- src/문제1000/Main.java
+++ src/문제1000/Main.java
@@ -1,15 +1,20 @@
 package 문제1000;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
 
+        //TEST용 코드
+//        String istest = "1 2";
+//        InputStream is = new ByteArrayInputStream(istest.getBytes());
+//        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String str = br.readLine();
+        String[] str = br.readLine().split(" ");
 
+        System.out.println(Integer.parseInt(str[0]) + Integer.parseInt(str[1]));
     }
 }
