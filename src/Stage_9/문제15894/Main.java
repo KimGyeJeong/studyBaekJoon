74abd6f363de384ee4dbc4fd26dc74b959d1fda6
KimGyeJeong <pmwkdo@gmail.com>
2023-11-06 21:21:51 +0900

    #JAVA #15894

---
+++ src/Stage_9/문제15894/Main.java
@@ -0,0 +1,27 @@
+package Stage_9.문제15894;
+
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        //TEST용 코드
+//        String istest = "4";
+//
+//        InputStream is = new ByteArrayInputStream(istest.getBytes());
+//        BufferedReader br = new BufferedReader(new InputStreamReader(is));
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        long square = Long.parseLong(br.readLine());
+        
+        bw.write(String.valueOf(square * 4));
+        
+        
+        br.close();
+
+        bw.flush();
+        bw.close();
+    }
+}
